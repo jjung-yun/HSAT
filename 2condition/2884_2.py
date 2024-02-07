aba5df026a01317bb29a9a9161a411c407fbe521
DESKTOP-0ADE0L6\User <y991102@khu.ac.kr>
2024-02-07 15:20:49 +0900

    2

--- 2condition/2884_2.py
+++ 2condition/2884_2.py
@@ -1,8 +1,7 @@
-import datetime
+import sys, datetime
 
 # 사용자로부터 시간과 분을 입력받습니다.
-hour = int(input("시간을 입력해주세요: "))
-minute = int(input("분을 입력해주세요: "))
+hour,minute=map(int,sys.stdin.readline().split())
 
 # 입력받은 시간과 분으로 datetime 오브젝트를 생성합니다.
 time = datetime.datetime(2024, 2, 6, hour, minute)
@@ -11,4 +10,6 @@ time = datetime.datetime(2024, 2, 6, hour, minute)
 new_time = time - datetime.timedelta(minutes=45)
 
 # 결과를 출력합니다.
-print("{} {}".format(new_time.hour, new_time.minute))
+#print("{} {}".format(new_time.hour, new_time.minute))
+print(new_time.hour, new_time.minute)
+# datetime모듈의 datetime 클래스 객체는 hour과 minute으로 뽑을 수 있으나, timedelta객체는 초로만 뽑을 수 있음.
