fec4b9c4352a9dc088acca7e9c92e8225eb45d7d
jjung-yun <y991102@khu.ac.kr>
2024-03-07 17:47:31 +0900

    d

---
+++ 2_백준단계별풀이/6_심화/1157_dictionary.py
@@ -0,0 +1,18 @@
+#1157 위 문제 딕셔너리 풀이
+word=input().upper()
+
+# 딕셔너리를 사용하여 단어의 빈도 세기
+word_count={}
+for i in word:
+    if i in word_count:
+        word_count[i]+=1
+    else:
+        word_count[i]=1
+
+# word_counts 딕셔너리의 가장 큰 value를 가지는 key를 출력
+max_key=max(word_count,key=word_count.get)
+max_key=max(word_count) # value들 중 최대값을 가진 key
+
+# 최대값이 여러개일 때는 ? 출력
+max_value=max(word_count.values()) # 전체 value들 리스트에서 최대값
+
